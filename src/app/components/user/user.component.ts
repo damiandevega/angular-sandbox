8359668294c572f29e9c3a0cf572084ce6affbd4
Damian de Vega <damiandevega@gmail.com>
2019-01-22 15:52:27 -0500

    learning about types in typescript

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -8,23 +8,47 @@ import { Component } from '@angular/core';
 
 export class UserComponent {
   // properties
-  firstName = 'Damian';
-  lastName = 'de Vega';
-  age = 30;
+  firstName: string;
+  lastName: string;
+  age: number;
+  address;
+
+  foo: any;
+  hasKids: boolean;
+  numberArray: number[];
+  stringArray: string[];
+  mixedArray: any[];
+  myTuple: [string, number, boolean];
+  unusable: void;
+  u: undefined;
+  n: null;
 
   // methods
   constructor() {
-    //this.sayHello()
-    console.log(this.age);
-    this.hasBirthday();
-    console.log(this.age);
+    this.firstName = 'Damian';
+    this.lastName = 'de Vega';
+    this.age = 30;
+
+    this.address = {
+      street: '40 Main Street',
+      city: 'New York',
+      state: 'NY'
+    }
+
+    this.foo = true;
+    this.hasKids = false;
+    this.numberArray = [1, 2, 3];
+    this.stringArray = ['hello', 'world'];
+    this.mixedArray = [true, 1, '2'];
+    this.myTuple = ['hello', 1, false];
+    this.unusable = undefined;
+    this.u = undefined;
+    this.n = null;
+
+    console.log(this.addNumbers(2, 3));
   }
 
-  sayHello() {
-    console.log(`Hello ${this.firstName}`)
-  }
-
-  hasBirthday() {
-    this.age += 1;
+  addNumbers(num1: number, num2: number): number {
+    return num1 + num2;
   }
 }
