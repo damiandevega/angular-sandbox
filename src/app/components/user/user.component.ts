d335952ed283fc9e6fd9d1619ea5d31e4a94b1c8
Damian de Vega <damiandevega@gmail.com>
2019-01-22 14:23:25 -0500

    learning about properties and methods in angular

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -7,5 +7,24 @@ import { Component } from '@angular/core';
 })
 
 export class UserComponent {
+  // properties
+  firstName = 'Damian';
+  lastName = 'de Vega';
+  age = 30;
 
+  // methods
+  constructor() {
+    //this.sayHello()
+    console.log(this.age);
+    this.hasBirthday();
+    console.log(this.age);
+  }
+
+  sayHello() {
+    console.log(`Hello ${this.firstName}`)
+  }
+
+  hasBirthday() {
+    this.age += 1;
+  }
 }
