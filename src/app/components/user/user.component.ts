9923629100d4898a0052a12fd2a4db313269ce9b
Damian de Vega <damiandevega@gmail.com>
2019-01-31 22:38:01 -0500

    added user model

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { User } from '../../models/User';
+
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
@@ -8,47 +10,19 @@ import { Component } from '@angular/core';
 
 export class UserComponent {
   // properties
-  firstName: string;
-  lastName: string;
-  age: number;
-  address;
-
-  foo: any;
-  hasKids: boolean;
-  numberArray: number[];
-  stringArray: string[];
-  mixedArray: any[];
-  myTuple: [string, number, boolean];
-  unusable: void;
-  u: undefined;
-  n: null;
+  user: User;
 
   // methods
   constructor() {
-    this.firstName = 'Damian';
-    this.lastName = 'de Vega';
-    this.age = 30;
-
-    this.address = {
-      street: '40 Main Street',
-      city: 'New York',
-      state: 'NY'
+    this.user = {
+      firstName: 'John',
+      lastName: 'Doe',
+      age: 30,
+      address: {
+        street: '40 Main st',
+        city: 'New York',
+        state: 'NY'
+      }
     }
-
-    this.foo = true;
-    this.hasKids = false;
-    this.numberArray = [1, 2, 3];
-    this.stringArray = ['hello', 'world'];
-    this.mixedArray = [true, 1, '2'];
-    this.myTuple = ['hello', 1, false];
-    this.unusable = undefined;
-    this.u = undefined;
-    this.n = null;
-
-    console.log(this.addNumbers(2, 3));
-  }
-
-  addNumbers(num1: number, num2: number): number {
-    return num1 + num2;
   }
 }
