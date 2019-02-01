ad7c5590747c896eb1d30df7b3d9966e77bc25ea
Damian de Vega <damiandevega@gmail.com>
2019-02-01 09:29:27 -0500

    learned about ngFor

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/User';
 
 @Component({
   selector: 'app-users',
@@ -6,11 +7,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  users: User[];
 
   constructor() { }
 
   ngOnInit() {
-    console.log('init...');
+    this.users = [
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        age: 30,
+        address: {
+          street: '40 Main st',
+          city: 'New York',
+          state: 'NY'
+        }
+      },
+      {
+        firstName: 'Kevin',
+        lastName: 'Johnson',
+        age: 29,
+        address: {
+          street: '343 Twain st',
+          city: 'Newark',
+          state: 'NJ'
+        }
+      },
+      {
+        firstName: 'Stephanie',
+        lastName: 'Williams',
+        age: 26,
+        address: {
+          street: '7777 Mill st',
+          city: 'Miami',
+          state: 'FL'
+        }
+      }
+    ]
+
+    this.addUser({
+      firstName: 'David',
+      lastName: 'Jackson',
+      age: 22,
+      address: {
+        street: '12 Wake st',
+        city: 'Boson',
+        state: 'MA'
+      }
+    })
+  }
+
+  addUser(user: User) {
+    this.users.push(user);
   }
 
 }
